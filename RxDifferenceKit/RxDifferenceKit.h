e16f0ed180aac70e1ffecabe332f67c47e7e0d78
Fumito Ito <fumito.ito@nex.nikkei.com>
2018-09-28 11:54:34 +0900

    Initial Commit

---
+++ RxDifferenceKit/RxDifferenceKit.h
@@ -0,0 +1,19 @@
+//
+//  RxDifferenceKit.h
+//  RxDifferenceKit
+//
+//  Created by Fumito Ito on 2018/09/28.
+//  Copyright © 2018年 Fumito Ito. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for RxDifferenceKit.
+FOUNDATION_EXPORT double RxDifferenceKitVersionNumber;
+
+//! Project version string for RxDifferenceKit.
+FOUNDATION_EXPORT const unsigned char RxDifferenceKitVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <RxDifferenceKit/PublicHeader.h>
+
+
